fcb8426b843c8c3cb81d627fee4fa2dbb975ca40
Tejas Dalvi <92875852+Tejas-358@users.noreply.github.com>
2023-02-28 20:44:13 +0530

    Update Main.java

--- Assignmnet 2/Assignmnet 2.1/src/Main.java
+++ Assignmnet 2/Assignmnet 2.1/src/Main.java
@@ -1,30 +1,44 @@
 import java.lang.*;
 import java.util.*;
-public class EOArray {
+
+public class Main {
     public static void main(String[] args)
     {
+        // Initialize variables and ArrayLists
         int n, j=1;
         ArrayList<Integer> even = new ArrayList<Integer>();
         ArrayList<Integer> odd = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
+
+        // Prompt user to enter count of numbers
         System.out.println("Enter Count of numbers : ");
         int c  = sc.nextInt();
+
+        // Prompt user to enter the numbers
         System.out.println("Enter numbers : ");
         for(int i = 0;i<c;i++)
         {
+            // Read the number from the user
             n = sc.nextInt();
+
+            // Check if the number is even or odd
             if(n%2==0)
             {
+                // Add even number to the even ArrayList
                 even.add(n);
             }
             else
             {
+                // Add odd number to the odd ArrayList
                 odd.add(n);
             }
         }
+
+        // Print the even ArrayList
         System.out.println("Even Array : ");
         System.out.println(even);
 
+        // Print the odd ArrayList
         System.out.println("Odd Array : ");
         System.out.println(odd);
     }
