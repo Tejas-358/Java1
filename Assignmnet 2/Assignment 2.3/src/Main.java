6c5bfbd3e2f10c607b5b51d479d276979c8cc100
Tejas Dalvi <92875852+Tejas-358@users.noreply.github.com>
2023-02-28 20:51:28 +0530

    Update Main.java

--- Assignmnet 2/Assignment 2.3/src/Main.java
+++ Assignmnet 2/Assignment 2.3/src/Main.java
@@ -3,46 +3,68 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        // Create an instance of the ATA class
         ATA obj = new ATA();
+        
+        // Call the ATAL method to get an ArrayList
         ArrayList l = obj.ATAL();
+        
+        // Call the ALTA method and pass the ArrayList as an argument
         obj.ALTA(l);
     }
 }
+
 class ATA {
+    // Declare a private ArrayList to store the elements
     private ArrayList j;
 
+    // This method creates an ArrayList, adds elements to it, and returns it
     ArrayList ATAL() {
+        // Create an array of strings
         String[] names = {"A", "B", "C", "D", "E"};
+        
+        // Create an ArrayList of strings and add the elements from the array
         ArrayList<String> a1 = new ArrayList<String>();
         for (int i = 0; i < names.length; i++) {
             a1.add(names[i]);
         }
+        
+        // Print the original ArrayList
         System.out.println("Original ArrayList : ");
         for (int i = 0; i < names.length; i++) {
             System.out.print(a1.get(i) + " ");
         }
-
+        
+        // Add more elements to the ArrayList
         a1.add("X");
         a1.add("Y");
         a1.add("Z");
 
+        // Print the new ArrayList
         System.out.println("\nNew ArrayList : ");
         for (int i = 0; i < a1.size(); i++) {
             System.out.print(a1.get(i) + " ");
         }
+        
+        // Return the ArrayList
         return (a1);
     }
 
+    // This method receives an ArrayList as an argument and converts it to an array of strings
     void ALTA(ArrayList j) {
+        // Set the private ArrayList to the one passed as an argument
         this.j = j;
+        
+        // Create an array of strings
         String[] str = new String[5];
 
-        System.out.println("\nArray : ");
-
+        // Copy the elements of the ArrayList to the array of strings
         for (int i = 0; i < str.length; i++) {
             str[i]=(String) j.get(i);
         }
 
+        // Print the array of strings
+        System.out.println("\nArray : ");
         for (int i = 0; i < str.length; i++) {
             System.out.print(str[i] + " ");
         }
