458a8d4a32a307ed1c3f870d1c8e556287ef676f
Tejas Dalvi <92875852+Tejas-358@users.noreply.github.com>
2023-02-28 19:47:40 +0530

    Update Main.java

--- Assignmnet 2/Assignmnet 2.1/src/Main.java
+++ Assignmnet 2/Assignmnet 2.1/src/Main.java
@@ -1,74 +1,31 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
+import java.lang.*;
+import java.util.*;
+public class EOArray {
+    public static void main(String[] args)
+    {
+        int n, j=1;
+        ArrayList<Integer> even = new ArrayList<Integer>();
+        ArrayList<Integer> odd = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
-        int[] even, odd;
-        int n;
-
-        // prompt user for number of elements
-        System.out.print("Enter number of elements: ");
-        n = sc.nextInt();
-
-        // initialize arrays with size n
-        even = new int[n];
-        odd = new int[n];
-
-        // fill arrays with numbers from user
-        fillArrays(even, odd, n, sc);
-
-        // print even numbers
-        System.out.print("Even numbers: ");
-        printArray(even, countEven(even));
-        System.out.println();
-
-        // print odd numbers
-        System.out.print("Odd numbers: ");
-        printArray(odd, countOdd(odd));
-    }
-
-    // method to fill arrays with numbers from user
-    public static void fillArrays(int[] even, int[] odd, int n, Scanner sc) {
-        System.out.println("Enter " + n + " elements:");
-        int i, j = 0, k = 0;
-        for (i = 0; i < n; i++) {
-            int num = sc.nextInt();
-
-            // check if number is even
-            if (num % 2 == 0) {
-                even[j++] = num;
-            } else {
-                odd[k++] = num;
+        System.out.println("Enter Count of numbers : ");
+        int c  = sc.nextInt();
+        System.out.println("Enter numbers : ");
+        for(int i = 0;i<c;i++)
+        {
+            n = sc.nextInt();
+            if(n%2==0)
+            {
+                even.add(n);
+            }
+            else
+            {
+                odd.add(n);
             }
         }
-    }
+        System.out.println("Even Array : ");
+        System.out.println(even);
 
-    // method to print array
-    public static void printArray(int[] arr, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    // method to count number of even numbers in array
-    public static int countEven(int[] arr) {
-        int count = 0;
-        for (int num : arr) {
-            if (num % 2 == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    // method to count number of odd numbers in array
-    public static int countOdd(int[] arr) {
-        int count = 0;
-        for (int num : arr) {
-            if (num % 2 != 0) {
-                count++;
-            }
-        }
-        return count;
+        System.out.println("Odd Array : ");
+        System.out.println(odd);
     }
 }
